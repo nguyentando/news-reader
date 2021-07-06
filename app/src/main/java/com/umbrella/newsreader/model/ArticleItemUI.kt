753ee6c0355d428fd721aaa15f7c814afc70ff3f
Do Nguyen <donguyen.official@gmail.com>
2021-07-06 23:39:31 +0700

    Smart Article Detail - support HTML tag for text type, parse the text in background threat. Update UI

--- app/src/main/java/com/umbrella/newsreader/model/ArticleItemUI.kt
+++ app/src/main/java/com/umbrella/newsreader/model/ArticleItemUI.kt
@@ -1,8 +1,10 @@
 package com.umbrella.newsreader.model
 
+import android.text.Spanned
+
 sealed class ArticleItemUI {
     data class Title(val text: String) : ArticleItemUI()
     data class Description(val text: String) : ArticleItemUI()
-    data class Text(val text: String) : ArticleItemUI()
+    data class Text(val text: Spanned) : ArticleItemUI()
     data class Image(val url: String, val width: Int, val height: Int) : ArticleItemUI()
 }
--- app/src/main/java/com/umbrella/newsreader/view/articledetail/smart/ArticleDetailSmartViewModel.kt
+++ app/src/main/java/com/umbrella/newsreader/view/articledetail/smart/ArticleDetailSmartViewModel.kt
@@ -1,27 +1,34 @@
 package com.umbrella.newsreader.view.articledetail.smart
 
+import android.os.Build
+import android.text.Html
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.umbrella.data.model.article.ArticleBody
 import com.umbrella.data.model.article.ArticleHeader
-import com.umbrella.data.util.onError
-import com.umbrella.data.util.onSuccess
+import com.umbrella.data.util.data
+import com.umbrella.domain.di.IoDispatcher
 import com.umbrella.domain.usecase.article.GetArticleDetailParam
 import com.umbrella.domain.usecase.article.GetArticleDetailUseCase
 import com.umbrella.newsreader.model.ArticleItemUI
+import com.umbrella.newsreader.util.HtmlTagHandler
 import com.umbrella.newsreader.util.SavedStateHandleManager
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
 class ArticleDetailSmartViewModel @Inject constructor(
     savedStateHandle: SavedStateHandle,
     savedStateHandleManager: SavedStateHandleManager,
-    private val getArticleDetailUseCase: GetArticleDetailUseCase
+    private val getArticleDetailUseCase: GetArticleDetailUseCase,
+    private val htmlTagHandler: HtmlTagHandler,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
 ) : ViewModel() {
     private val articleHeader = savedStateHandleManager.getNavArgs<ArticleHeader>(savedStateHandle)
     private val headerUIList = articleHeader.let {
@@ -38,22 +45,30 @@ class ArticleDetailSmartViewModel @Inject constructor(
     // then call the API to get the body
     init {
         viewModelScope.launch {
-            getArticleDetailUseCase(GetArticleDetailParam(articleHeader.id))
-                .onSuccess {
-                    it.let {
-                        val data = listOf(headerUIList)
-                        it.body.forEach {
-                            when (it) {
-                                is ArticleBody.Image -> ArticleItemUI.Image(it.url, it.width, it.height)
-                                is ArticleBody.Text -> ArticleItemUI.Text(it.text)
+            getArticleDetailUseCase(GetArticleDetailParam(articleHeader.id)).data?.let {
+                val data = headerUIList.toMutableList()
+                it.body.map {
+                    when (it) {
+                        is ArticleBody.Image -> ArticleItemUI.Image(it.url, it.width, it.height)
+                        is ArticleBody.Text -> {
+                            // parse the text in bgThreat
+                            withContext(ioDispatcher) {
+                                val body = if (Build.VERSION.SDK_INT >= 24) {
+                                    Html.fromHtml(it.text, Html.FROM_HTML_MODE_LEGACY, null, htmlTagHandler)
+                                } else {
+                                    Html.fromHtml(it.text, null, htmlTagHandler)
+                                }
+                                ArticleItemUI.Text(body)
                             }
                         }
-                        // TODO: 06/07/2021 handle footer
                     }
+                }.let {
+                    data.addAll(it)
                 }
-                .onError {
-                    // TODO: 06/07/2021 handle error view }
-                }
+                // TODO: 06/07/2021 handle footer
+                _data.value = data
+            }
+            // TODO: 06/07/2021 handle error view }
         }
     }
 }
